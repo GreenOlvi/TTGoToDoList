ce646e4d6620240d7ad62e35c94d128c2e554809
Piotr Szulc <szulo123@gmail.com>
2021-11-23 19:20:01 +0100

    Store and recall wifi config

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,19 @@
 
 GxEPD2_BW<GxEPD2_213_B72, GxEPD2_213_B72::HEIGHT> display(GxEPD2_213_B72(CS_PIN, DC_PIN, RST_PIN, BUSY_PIN));
 
+nvs_handle NvsHandle;
+
+IPAddress ip;
+IPAddress gateway;
+IPAddress subnet;
+IPAddress dns;
+uint8_t channel;
+uint8_t ap_mac[6];
+
 TrelloClient trello(TRELLO_API_KEY, TRELLO_TOKEN);
 
 String ListTitle;
 LinkedList<TrelloCheckitem> ListItems;
-int32_t RestartCounter = 0;
 
 void drawGrid() {
     for (int i = 0; i < display.height(); i += 10) {
@@ -35,23 +43,13 @@ void setCursorWithOffser(int16_t x, int16_t y) {
     display.setCursor(x - FONT_OFFSET_X, y - FONT_OFFSET_Y);
 }
 
-void initNvs() {
-
-    auto err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        Serial.printf("Erased flash");
-        err = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(err);
-
-    nvs_handle NvsHandle;
-    err = nvs_open("storage", NVS_READWRITE, &NvsHandle);
-
-    err = nvs_get_i32(NvsHandle, "restartCounter", &RestartCounter);
+void loadIpAddress(const char *key, IPAddress &value) {
+    uint32_t val;
+    auto err = nvs_get_u32(NvsHandle, key, &val);
     switch (err) {
         case ESP_OK:
-            Serial.printf("Restart counter = %d\n", RestartCounter);
+            value = IPAddress(val);
+            Serial.printf("Loaded [%s] = %s\n", key, value.toString().c_str());
             break;
         case ESP_ERR_NVS_NOT_FOUND:
             Serial.printf("Not initialized yet\n");
@@ -60,23 +58,70 @@ void initNvs() {
             Serial.printf("Error (%s) reading\n", esp_err_to_name(err));
             break;
     }
+}
 
-    RestartCounter++;
+void initNvs() {
+    auto err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        Serial.printf("Erased flash");
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
 
-    err = nvs_set_i32(NvsHandle, "restartCounter", RestartCounter);
-    err = nvs_commit(NvsHandle);
+    err = nvs_open("storage", NVS_READWRITE, &NvsHandle);
+
+    loadIpAddress("ip", ip);
+    loadIpAddress("gateway", gateway);
+    loadIpAddress("subnet", subnet);
+    loadIpAddress("dns", dns);
+    nvs_get_u8(NvsHandle, "channel", &channel);
+    Serial.printf("Loaded [channel] = %d\n", channel);
+    size_t macSize;
+    nvs_get_blob(NvsHandle, "ap_mac", &ap_mac, &macSize);
+    Serial.print("Loaded [ap_mac] = ");
+    for (int i = 0; i < macSize - 1; i++) {
+        Serial.printf("%02x:", ap_mac[i]);
+    }
+    Serial.printf("%02x\n", ap_mac[5]);
+}
+
+void storeWiFiData() {
+    if (!ip) {
+        Serial.println("Saving ip data");
+        nvs_set_u32(NvsHandle, "ip", WiFi.localIP());
+        nvs_set_u32(NvsHandle, "gateway", WiFi.gatewayIP());
+        nvs_set_u32(NvsHandle, "subnet", WiFi.subnetMask());
+        nvs_set_u32(NvsHandle, "dns", WiFi.dnsIP());
+        nvs_set_u8(NvsHandle, "channel", WiFi.channel());
+        nvs_set_blob(NvsHandle, "ap_mac", WiFi.BSSID(), sizeof(ap_mac));
+        nvs_commit(NvsHandle);
+    }
     nvs_close(NvsHandle);
 }
 
 void connectToWiFi() {
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
+    if (ip) {
+        WiFi.config(ip, gateway, subnet, dns);
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD, channel, ap_mac, true);
+    } else {
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     }
 
-    Serial.println("Connected to WiFi");
+    int retries = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        retries++;
+        delay(50);
+
+        if (retries == 100) {
+            ip = IPAddress();
+            WiFi.disconnect();
+            delay(10);
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        }
+    }
+
+    Serial.printf("Connected to WiFi after %lu\n", millis());
 }
 
 void LoadViewFromCard(trelloId_t cardId) {
@@ -105,6 +150,8 @@ void setup() {
     connectToWiFi();
     LoadViewFromCard(CARD_ID);
 
+    storeWiFiData();
+
     display.firstPage();
     do
     {
@@ -129,8 +176,6 @@ void setup() {
             y += 10;
         }
 
-        setCursorWithOffser(13, display.height() - 30);
-        display.printf("Restarts: %d", RestartCounter);
         setCursorWithOffser(13, display.height() - 20);
         display.print(WiFi.macAddress());
         setCursorWithOffser(13, display.height() - 10);
